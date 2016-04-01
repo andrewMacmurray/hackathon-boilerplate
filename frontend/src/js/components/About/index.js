024c6ddc19e52da91e3c89d88430a49c24b80f48
Andrew MacMurray <a.macmurray@icloud.com>
2016-04-01 14:54:15 +0100

    some comsetic changes, add rhino vector

--- frontend/src/js/components/About/index.js
+++ frontend/src/js/components/About/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
 
+const styles = {
+  textAlign: 'center',
+  img: {
+    outer: {
+      width: '80%',
+      maxWidth: '400px',
+      margin: '4em auto'
+    },
+    inner: {
+      width: '100%'
+    }
+  }
+}
+
 export default (props) => {
   return (
-    <Grid>
+    <Grid style={styles}>
       <Row>
         <Col xs={12}>
-          <p>this is the about content</p>
+          <div style={styles.img.outer}>
+            <img style={styles.img.inner} src='img/rhino.png' />
+          </div>
+          <p>Your app goes here...</p>
         </Col>
       </Row>
     </Grid>
