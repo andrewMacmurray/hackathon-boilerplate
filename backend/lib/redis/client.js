f3b2dc29b250f440d5c2ed31d8d5cf3980ead471
Elias <elias0789@gmail.com>
2016-04-15 12:01:27 +0100

    Export function that returns client instead of client itself to help with testability.

--- backend/lib/redis/client.js
+++ backend/lib/redis/client.js
@@ -1,11 +1,17 @@
 import redis from 'redis'
+import bluebird from 'bluebird'
 
-const DB_URL = process.env.REDIS_URL || 'redis://localhost:6379'
-const DB_NUM = process.env.REDIS_DB || 0
-const client = redis.createClient(DB_URL)
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
 
-client.select(DB_NUM, () => {
-  console.log('Connected to Redis database num ', DB_NUM, ' on ', DB_URL)
-})
+export default (opts) => {
 
-export default client
+  const config = {
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    db: process.env.REDIS_DB || 0
+  }
+
+  if (opts && opts.env === 'TEST') config.db = 5
+
+  return redis.createClient(config)
+}
