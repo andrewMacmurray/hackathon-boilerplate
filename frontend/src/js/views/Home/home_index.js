10c9777d272a6e8986f505ae3575fb8bf2294e75
Andrew <a.macmurray@icloud.com>
2016-04-21 10:14:29 +0100

    switch about and home content

--- frontend/src/js/views/Home/home_index.js
+++ frontend/src/js/views/Home/home_index.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Jumbotron } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
 
-export default () => {
-  return (
-    <div className='home'>
-      <h1>Hackathon Boilerplate</h1>
-    </div>
-  )
+const styles = {
+  textAlign: 'center',
+  img: {
+    outer: {
+      width: '80%',
+      maxWidth: '400px',
+      margin: '4em auto'
+    },
+    inner: {
+      width: '100%'
+    }
+  }
+}
+
+export default class Home extends React.Component {
+  checkCookie () {
+    return document.cookie.indexOf('reactCookie') > -1
+  }
+
+  render () {
+    return (
+      <Grid style={styles}>
+        <Row>
+          <Col xs={12}>
+            <div style={styles.img.outer}>
+              <img style={styles.img.inner} src='img/rhino.png' />
+            </div>
+            <p>Your app goes here...</p>
+            {this.checkCookie() ? '' : <a href='/login-with-twitter'>login with twitter</a>}
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 }
